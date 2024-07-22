416fa605f8b0aa93044efe01973668f28237fa7a
warlock <gaurav.rana@coderootz.com>
2024-07-22 14:49:18 +0530

    react eslint airbnb prettier typescript setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig, Plugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import eslint from 'vite-plugin-eslint';
+
+const root = resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), eslint() as Plugin],
+  resolve: {
+    alias: {
+      '@': resolve(root),
+    },
+  },
+});
