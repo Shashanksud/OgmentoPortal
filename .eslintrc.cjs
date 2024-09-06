9a0e31de0b4debff26e601d9c775558890d0d464
warlock <gaurav.rana@coderootz.com>
2024-09-06 13:32:53 +0530

    added env, renamed title in index.html, added path resolver in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -49,4 +49,12 @@ module.exports = {
       },
     ],
   },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
