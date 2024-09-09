5c30b4ebf82c018b801760e9c99a3c7b58ab4c71
warlock <gaurav.rana@coderootz.com>
2024-09-09 12:36:15 +0530

    fixed redundancy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import './App.css';
 import NavBar from './component/NavBar';
-import AllRoutes from './routes/AllRoutes';
 
 function App() {
-  return (
-    <div>
-      <NavBar>
-        <AllRoutes />
-      </NavBar>
-    </div>
-  );
+  return <NavBar />;
 }
 
 export default App;
