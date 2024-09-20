d4ec02f4e7c620485f0a81c014a63bd8491bb6b8
warlock <gaurav.rana@coderootz.com>
2024-09-20 15:20:18 +0530

    added: multi theme configuration,  multi theme support, typography, shadows

--- src/theme/typography.tsx
+++ src/theme/typography.tsx
@@ -13,32 +13,50 @@ const Typography = (
   fontFamily,
   h6: {
     fontWeight: 500,
-    color: theme.palette.grey[900],
+    color:
+      theme.palette.mode === 'dark'
+        ? theme.palette.grey[600]
+        : theme.palette.grey[900],
     fontSize: '0.75rem',
   },
   h5: {
     fontSize: '0.875rem',
-    color: theme.palette.grey[900],
+    color:
+      theme.palette.mode === 'dark'
+        ? theme.palette.grey[600]
+        : theme.palette.grey[900],
     fontWeight: 500,
   },
   h4: {
     fontSize: '1rem',
-    color: theme.palette.grey[900],
+    color:
+      theme.palette.mode === 'dark'
+        ? theme.palette.grey[600]
+        : theme.palette.grey[900],
     fontWeight: 600,
   },
   h3: {
     fontSize: '1.25rem',
-    color: theme.palette.grey[900],
+    color:
+      theme.palette.mode === 'dark'
+        ? theme.palette.grey[600]
+        : theme.palette.grey[900],
     fontWeight: 600,
   },
   h2: {
     fontSize: '1.5rem',
-    color: theme.palette.grey[900],
+    color:
+      theme.palette.mode === 'dark'
+        ? theme.palette.grey[600]
+        : theme.palette.grey[900],
     fontWeight: 700,
   },
   h1: {
     fontSize: '2.125rem',
-    color: theme.palette.grey[900],
+    color:
+      theme.palette.mode === 'dark'
+        ? theme.palette.grey[600]
+        : theme.palette.grey[900],
     fontWeight: 700,
   },
   subtitle1: {
