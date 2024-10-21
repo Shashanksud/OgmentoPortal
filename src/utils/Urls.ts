87571c614a895cf96fbf90ed824f67fa156b62b4
AnkitSharma745 <ankitaksharma9763@gmail.com>
2024-10-21 10:51:47 +0530

    refactor: update url endpoint

--- src/utils/Urls.ts
+++ src/utils/Urls.ts
@@ -2,4 +2,4 @@ export const BASE_URL = 'https://ogmento-apis.azurewebsites.net';
 const userAPIBaseUrl = 'https://ogmento-apis.azurewebsites.net/api/Users/';
 export const getUserDetails = `${userAPIBaseUrl}/getUserDetails`;
 
-export const categoryEndpoint = '/api/category';
+export const categoryEndpoint = '/api/Category';
