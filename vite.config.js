ad1db7a75cf463b309b9424f462c75c4aad0aae4
Shashank Sood <shashank.sood@filuet.com>
2024-11-20 12:27:24 +0530

    Commit made from VS Code Azure Static Web Apps

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import eslint from 'vite-plugin-eslint2';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+var root = resolve(__dirname, 'src');
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), eslint({ fix: true }), basicSsl()],
+    resolve: {
+        alias: {
+            '@': resolve(root),
+        },
+    },
+    server: { port: 5174 },
+});
